fe642ad8ac4f6053832b64e6d040af80d20fc085
exzco <1027822561@qq.com>
2026-01-16 22:21:47 +0800

    2026-1-16_review_java

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "归档",
+	title: "wele",
 	subtitle: "wele",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
